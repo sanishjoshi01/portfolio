0253cfd711ae2f134f4e32a575ff49cc90e958c2
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-01-10 17:58:19 +0545

    working on responsive navigation

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ links.forEach(function (link) {
         link.classList.add('active');
     });
 });
+
+let menu = document.querySelector(".nav-bar");
+let menuBar = document.querySelector(".menu-bar");
+
+menu.addEventListener('click', function () {
+    if (menuBar.style.display = "block") {
+        menuBar.style.display = "none";
+    }
+    else {
+        menuBar.style.display = "block";
+    }
+});
