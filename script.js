73ffcd888314d5c1763f58d54fcd53301e2858b5
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-01-16 19:41:52 +0545

    added theme button

--- script.js
+++ script.js
@@ -10,30 +10,50 @@ links.forEach(function (link) {
     });
 });
 
-//menu show and hide for mobile
-let menu = document.querySelector(".left-nav");
-let menuBar = document.querySelector(".left-nav-content-one");
 
-menu.addEventListener('click', function () {
-    if (menuBar.style.display === "none") {
-        menu.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
-        menuBar.style.display = "block";
+
+//menu show and hide for mobile
+// let menu = document.querySelector(".left-nav");
+// let menuBar = document.querySelector(".left-nav-content-one");
+
+
+// menu.addEventListener('click', function () {
+//     if (menuBar.style.opacity === "1") {
+//         menuBar.style.opacity = "0";
+//         menu.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+//     }
+//     else {
+//         menu.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+//         menuBar.style.opacity = "1";
+//     }
+// });
+
+
+
+//dark mode light mode
+let themeBtn = document.querySelector(".nav-bar .theme-button");
+
+console.log(themeBtn);
+
+themeBtn.addEventListener('click', () => {
+    if (themeBtn.innerHTML === `<i class="fa-regular fa-sun"></i>`) {
+        themeBtn.innerHTML = `<i class="fa-solid fa-moon"></i>`;
     }
     else {
-        menuBar.style.display = "none";
-        menu.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+        themeBtn.innerHTML = `<i class="fa-regular fa-sun"></i>`;
     }
 });
 
-// TITLE DECORATION
 
+
+
+// TITLE DECORATION
 const headingDecor = document.querySelectorAll('.main-title-heading');
 
 console.log(headingDecor);
 headingDecor.forEach(heading => {
     heading.innerHTML = '&lt;&lt;&lt; ' + heading.textContent + ' &gt;&gt;&gt;';
 });
-
 
 
 
@@ -51,6 +71,7 @@ headingDecor.forEach(heading => {
 
 // let contHolder = ""
 // const projectImg = ["dara"];
+
 
 
 //loading on and off
